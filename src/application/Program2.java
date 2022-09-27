0e09d65d9408866b179c1eb45365477d0294fae2
Carlos Borges <kaca_master@hotmail.com>
2022-09-26 23:30:42 -0300

    insert implementation department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.List;
-import java.util.Scanner;
 
 import db.DB;
 import model.dao.DaoFactory;
@@ -12,7 +11,7 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartentDao();
 		
@@ -28,10 +27,12 @@ public class Program2 {
 			System.out.println(dep);
 		}
 	 
-		 
 		
+		 System.out.println("\n=== TEST 3: department insert ====");
+			Department newDepartment = new Department(null, "Administrativo");
+			departmentDao.insert(newDepartment);
+			System.out.println("Inserted! New department Id = " + newDepartment.getId());
 		
-		sc.close();
 		
 		DB.closeConnection();
 	}
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,13 +22,48 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 			}
 
 	@Override
-	public void insert(Department obj) {
-		// TODO Auto-generated method stub
+	public void insert(Department dep) {
+		
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO department "
+					+ "(Name) "
+					+ "VALUES "	
+					+ "(?)",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, dep.getName());
+			
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					dep.setId(id);
+				}
+				DB.closeResultSet(rs);
+			}
+			else {
+				throw new DbException("Unexpected error! No ows affected!");
+			}
+					
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+
 
 	}
 
 	@Override
-	public void update(Department obj) {
+	public void update(Department dep) {
 		// TODO Auto-generated method stub
 
 	}
